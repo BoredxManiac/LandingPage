f8be33becd4dab120e2a46d94384384b3946891f
BoredxManiac <Manicalsome.1@gmail.com>
2026-03-06 11:29:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="pages">
+                <div class="page">
+                    <h1>fdgxfjhxf</h1>
+                    <h1>OUDE PORTOFOLIO</h1>
+                    <img src="img/Screenshot 2024-10-10 183906.png" alt="yes" width="250px" height="200px">
+                    <a href="mijnportfolio.cherryblossomicetea.nl ">mijnportfolio</a>
+                </div>
+                <div class="page">
+                    <h1>NIEUWE PORTOFOLIO</h1>
+                    <img src="img/Screenshot 2025-03-14 113207.png" alt="yes" width="300px" height="200px">
+                    <a href="portfolio.cherryblossomicetea.nl ">portfolio</a>
+                </div>
+                <div class="page">    
+                        
+                    <h1>MY EERSTE WEBSITE</h1>
+                    <img src="img/Screenshot 2025-02-03 214918.png" alt="yes" width="290px" height="200px">
+                    <a href="firstweb.cherryblossomicetea.nl ">firstweb</a>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
