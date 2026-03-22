933eab09d6bd5946687f90f16a7e11fc73c85175
BoredxManiac <Manicalsome.1@gmail.com>
2026-03-22 20:40:14 +0100

    starting point

--- index.php
+++ index.php
@@ -1,32 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    <div class="wrapper">
-        <div class="pages">
-                <div class="page">
-                    <h1>fdgxfjhxf</h1>
-                    <h1>OUDE PORTOFOLIO</h1>
-                    <img src="img/Screenshot 2024-10-10 183906.png" alt="yes" width="250px" height="200px">
-                    <a href="mijnportfolio.cherryblossomicetea.nl ">mijnportfolio</a>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <?php
+        require_once('head.php');
+    ?>
+
+    <body>
+        <div class="wrapper">
+            <div class="position">
+                <div class="pages">
+                     <div class="page">
+                        <h2>NIEUWE PORTOFOLIO</h2>
+                        <a href="portfolio.cherryblossomicetea.nl "> <img src="img/cat.jpg" alt="yes" width="300px" height="200px"></a>
+                    </div>
+                    <div class="page">
+                        <h2>NIEUWE PORTOFOLIO</h2>
+                        <a href="portfolio.cherryblossomicetea.nl "> <img src="img/cat.jpg" alt="yes" width="300px" height="200px"></a>
+                    </div>
+                    <div class="page">
+                        <h2>NIEUWE PORTOFOLIO</h2>
+                        <a href="portfolio.cherryblossomicetea.nl "> <img src="img/cat.jpg" alt="yes" width="300px" height="200px"></a>
+                    </div>
+                     <div class="page">
+                        <h2>NIEUWE PORTOFOLIO</h2>
+                        <a href="portfolio.cherryblossomicetea.nl "> <img src="img/cat.jpg" alt="yes" width="300px" height="200px"></a>
+                    </div>
                 </div>
-                <div class="page">
-                    <h1>NIEUWE PORTOFOLIO</h1>
-                    <img src="img/Screenshot 2025-03-14 113207.png" alt="yes" width="300px" height="200px">
-                    <a href="portfolio.cherryblossomicetea.nl ">portfolio</a>
+                <div class="portfoliotxts">
+                    <div class="portfoliotxt">
+                        <h1>WHATS GOOD</h1>
+                    </div>
+                     <div class="portfoliotxtp">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, possimus omnis. <br>Voluptate, non fugit hic reprehenderit cupiditate amet voluptatum cumque ut, mollitia praesentium aspernatur quod officia.<br> Labore error sint officia!loydgyjdsghdghdvghxhjvcnhxzxzhjxcvhjxxgfxgxvc</p>
+                    </div>
                 </div>
-                <div class="page">    
-                        
-                    <h1>MY EERSTE WEBSITE</h1>
-                    <img src="img/Screenshot 2025-02-03 214918.png" alt="yes" width="290px" height="200px">
-                    <a href="firstweb.cherryblossomicetea.nl ">firstweb</a>
-                </div>
+            </div>
         </div>
-    </div>
+    </body>
+
+    </html>
 </body>
+
 </html>
